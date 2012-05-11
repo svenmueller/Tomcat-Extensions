ee40d2d35bd96c13b331f5917c894ff94d71ccdb
Sven Mueller <sven.mueller@commercetools.de>
2012-05-11 13:54:22 +0200

    Add check for 'unrenamed' ssl sesson, which needs to be renamed.

--- src/valve/SessionFixationValve.java
+++ src/valve/SessionFixationValve.java
@@ -1,14 +1,3 @@
-/*
- * commercetools GmbH
- * 
- * Copyright (c) 2006 commercetools
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of commercetools
- * ("Confidential Information").  You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with commercetools.
- */
 package valve;
 
 import java.io.IOException;
@@ -20,34 +9,52 @@ import org.apache.catalina.Session;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
+import org.apache.log4j.Logger;
 
 /**
- * This valve changes the session ID. It does not destroy the previous session,
- * rather it renames it so it is no longer found by that ID.
+ * This valve changes the session ID when a secure request is. It does not
+ * destroy the previous session, rather it renames it so it is no longer found
+ * by that ID.
  * 
  * @author sven mueller
  * @version $Revision:$
  */
 public class SessionFixationValve extends ValveBase {
 
+	private final static Logger LOG = Logger.getLogger(SessionFixationValve.class.getName());
+
+	private static final String SESSION_RENAMED = "session.renamed";
+
 	@Override
 	public void invoke(Request request, Response response) throws IOException, ServletException {
-		if (request.getSessionInternal() != null) {
+
+		// get existing session session
+		Session session = request.getSessionInternal();
+
+		// is there already a session?
+		if (session != null && request.getConnector().getSecure() && session.getSession().getAttribute(SESSION_RENAMED) == null) {
 
 			// get manager, needed to rename session
 			Manager manager = request.getContext().getManager();
 
-			// get existing session session
-			Session session = request.getSessionInternal();
+			String oldSessionId = session.getId();
 
 			// "rename" existing session
 			manager.changeSessionId(session);
 
 			// set session id to request
 			request.changeSessionId(session.getId());
-		}
-	}
 
+			// mark session as "renamed"
+			session.getSession().setAttribute(SESSION_RENAMED, true);
+
+			if (LOG.isDebugEnabled()) {
+				LOG.debug(String.format("Session renamed from '%s' to '%s'.", oldSessionId, session.getSession().getId()));
+			}
+		}
+
+		getNext().invoke(request, response);
+	}
 }
 
 /*
